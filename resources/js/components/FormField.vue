a3eb5e6461a3d318937a659ad7d558ca829923ff
Stef van Esch <stef@marshmallow.dev>
2021-07-28 15:28:41 +0200

    Update NPM

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,11 +1,16 @@
 <template>
-    <default-field :field="field" :full-width-content="true" :show-help-text="showHelpText">
+    <default-field
+        :field="field"
+        :full-width-content="true"
+        :show-help-text="showHelpText"
+    >
         <template slot="field">
-            <editor :id="field.id || field.attribute"
-                    v-model="value"
-                    :class="errorClasses"
-                    :placeholder="field.name"
-                    :init="options"
+            <editor
+                :id="field.id || field.attribute"
+                v-model="value"
+                :class="errorClasses"
+                :placeholder="field.name"
+                :init="options"
             ></editor>
 
             <p v-if="hasError" class="my-2 text-danger">
@@ -16,74 +21,84 @@
 </template>
 
 <script>
-import { FormField, HandlesValidationErrors } from 'laravel-nova'
-import Editor from '@tinymce/tinymce-vue'
+    import { FormField, HandlesValidationErrors } from "laravel-nova";
+    import Editor from "@tinymce/tinymce-vue";
 
-export default {
-    components: { Editor },
+    export default {
+        components: { Editor },
 
-    mixins: [FormField, HandlesValidationErrors],
+        mixins: [FormField, HandlesValidationErrors],
 
-    props: ['resourceName', 'resourceId', 'field'],
+        props: ["resourceName", "resourceId", "field"],
 
-    computed: {
-        options() {
-            let options = this.field.options
+        computed: {
+            options() {
+                let options = this.field.options;
 
-            this.darkThemeOn = localStorage.darkThemeOn === "true";
-            if (localStorage.darkThemeOn === "true") {
-                options['skin_url'] = '/vendor/tinymce/skins/ui/mm-dark';
-                options['content_css'] = '/vendor/tinymce/skins/content/mm-dark/content.css';
-            }
-
-            if (options.use_lfm) {
-                options['file_picker_callback'] = this.filePicker
-            }
-
-            return options
-        }
-    },
-
-    methods: {
-        /*
-         * Set the initial, internal value for the field.
-         */
-        setInitialValue() {
-          this.value = this.field.value || ''
-        },
-
-        /**
-         * Fill the given FormData object with the field's internal value.
-         */
-        fill(formData) {
-          formData.append(this.field.attribute, this.value || '')
-        },
-
-        /**
-         * Update the field's internal value.
-         */
-        handleChange(value) {
-          this.value = value
-        },
-
-        filePicker: function (callback, value, meta) {
-            let x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
-            let y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
-
-            let type = 'image' === meta.filetype ? 'Images' : 'Files';
-            let url  = this.options.path_absolute + this.options.lfm_url + '?editor=tinymce5&type=' + type;
-
-            tinymce.activeEditor.windowManager.openUrl({
-                url : url,
-                title : 'File manager',
-                width : x * 0.8,
-                height : y * 0.8,
-                onMessage: (api, message) => {
-                    callback(message.content);
+                this.darkThemeOn = localStorage.darkThemeOn === "true";
+                if (localStorage.darkThemeOn === "true") {
+                    options["skin_url"] = "/vendor/tinymce/skins/ui/mm-dark";
+                    options["content_css"] =
+                        "/vendor/tinymce/skins/content/mm-dark/content.css";
                 }
-            });
-        }
 
-    }
-}
+                if (options.use_lfm) {
+                    options["file_picker_callback"] = this.filePicker;
+                }
+
+                return options;
+            },
+        },
+
+        methods: {
+            /*
+             * Set the initial, internal value for the field.
+             */
+            setInitialValue() {
+                this.value = this.field.value || "";
+            },
+
+            /**
+             * Fill the given FormData object with the field's internal value.
+             */
+            fill(formData) {
+                formData.append(this.field.attribute, this.value || "");
+            },
+
+            /**
+             * Update the field's internal value.
+             */
+            handleChange(value) {
+                this.value = value;
+            },
+
+            filePicker: function(callback, value, meta) {
+                let x =
+                    window.innerWidth ||
+                    document.documentElement.clientWidth ||
+                    document.getElementsByTagName("body")[0].clientWidth;
+                let y =
+                    window.innerHeight ||
+                    document.documentElement.clientHeight ||
+                    document.getElementsByTagName("body")[0].clientHeight;
+
+                let type = "image" === meta.filetype ? "Images" : "Files";
+                let url =
+                    this.options.path_absolute +
+                    this.options.lfm_url +
+                    "?editor=tinymce5&type=" +
+                    type;
+
+                tinymce.activeEditor.windowManager.openUrl({
+                    url: url,
+                    title: "File manager",
+                    width: x * 0.8,
+                    height: y * 0.8,
+                    onMessage: (api, message) => {
+                        callback(message.content);
+                    },
+                });
+            },
+        },
+    };
 </script>
