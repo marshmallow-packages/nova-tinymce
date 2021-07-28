a376acf739a389bec766844947a00684c3b89d64
Stef van Esch <stef@marshmallow.dev>
2021-07-28 15:46:41 +0200

    reverted npm depencencies

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,24 +1,9 @@
-// import IndexNovaTinyMCEField from "./components/IndexField";
-// import DetailNovaTinyMCEField from "./components/DetailField";
-// import FormNovaTinyMCEField from "./components/FormField";
+import IndexNovaTinyMCEField from "./components/IndexField";
+import DetailNovaTinyMCEField from "./components/DetailField";
+import FormNovaTinyMCEField from "./components/FormField";
 
-Nova.booting((Vue, router, store) => {
-    Vue.component(
-        "index-Nova-TinyMCE",
-        require("./components/IndexField.vue").default
-    );
-    Vue.component(
-        "detail-Nova-TinyMCE",
-        require("./components/DetailField.vue").default
-    );
-    Vue.component(
-        "form-Nova-TinyMCE",
-        require("./components/FormField.vue").default
-    );
+Nova.booting((Vue, router) => {
+    Vue.component("index-Nova-TinyMCE", IndexNovaTinyMCEField);
+    Vue.component("detail-Nova-TinyMCE", DetailNovaTinyMCEField);
+    Vue.component("form-Nova-TinyMCE", FormNovaTinyMCEField);
 });
-
-// Nova.booting((Vue, router) => {
-//     Vue.component("index-Nova-TinyMCE", IndexNovaTinyMCEField).default;
-//     Vue.component("detail-Nova-TinyMCE", DetailNovaTinyMCEField).default;
-//     Vue.component("form-Nova-TinyMCE", FormNovaTinyMCEField).default;
-// });
